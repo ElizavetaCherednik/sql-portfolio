0215df9a66593943b3eae1486b5f1e4d4c79b534
ElizavetaCherednik <cherednik06@bk.ru>
2026-04-10 13:41:39 +0300

    Create grouping_aggregates.sql
    Для каждого дня недели в период с 24 августа по 6 сентября 2022 года включительно в таблице user_actions посчитано:
    Общее количество оформленных заказов.
    Общее количество отменённых заказов.
    Общее количество неотменённых заказов (т.е. доставленных).
    Долю неотменённых заказов в общем числе заказов (success rate).

---
+++ grouping_aggregates.sql
@@ -0,0 +1,15 @@
+SELECT
+    DATE_PART('isodow', time)::int AS weekday_number,
+    TO_CHAR(time, 'Dy') AS weekday,
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order') AS created_orders,
+    COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order') AS canceled_orders,
+    COUNT(DISTINCT order_id)  FILTER (WHERE action = 'create_order') - COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order') AS actual_orders,
+    ROUND(
+        (COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order') - COUNT(DISTINCT order_id) FILTER (WHERE action = 'cancel_order'))::decimal / 
+        COUNT(DISTINCT order_id) FILTER (WHERE action = 'create_order'),
+        3
+    ) AS success_rate
+FROM user_actions
+WHERE time BETWEEN '2022-08-24' AND '2022-09-06 23:59:59'
+GROUP BY weekday_number, weekday
+ORDER BY weekday_number
